5cffdb8acad7b8501902b22025866443bd3c69cc
axelhamburch <axelhamburch@protonmail.com>
2024-05-31 21:13:56 +0200

    fck

--- include/lightning_atm.h
+++ include/lightning_atm.h
@@ -14,7 +14,7 @@
 // ###########    USER ACTION   ###########
 // ########################################
 // Generate and copy in LNbits with the LNURLDevice extension the string for the ATM and paste it here:
-const String lnurlDeviceString = "https://lnbits.ereignishorizont.xyz/lnurldevice/api/v1/lnurl/QuUjK,fCfdNwwFUAkK9bqyKwFHNL,EUR";
+const String lnurlDeviceString = "https://lnbits.ereignishorizont.xyz/lnurldevice/api/v1/lnurl/QuUjK,fCfdNwwFUAkK9bqyKwFHNL,EUR               ";
 // #################### EXAMPLE:  https://legend.lnbits.com/lnurldevice/api/v1/lnurl/idexample,keyexample,EUR
 // ########################################
 // ########################################
